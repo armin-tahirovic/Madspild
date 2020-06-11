ffd90c232c67238c678e49260d59355ca8c4db10
armin <armin2013@live.dk>
2020-06-11 21:20:36 +0200

    Finished firebase authentication

--- app/src/main/java/com/example/madspild/firebase/FirebaseActivity.java
+++ app/src/main/java/com/example/madspild/firebase/FirebaseActivity.java
@@ -2,16 +2,14 @@ package com.example.madspild.firebase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.madspild.MainActivity;
 import com.example.madspild.R;
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -26,10 +24,22 @@ public class FirebaseActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    checkIfSignedIn();
+    setContentView(R.layout.firebase_sign_in);
   }
 
-  public void signIn() {
+  private void checkIfSignedIn() {
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    if (user != null)
+      goToMainActivity();
+  }
+
+  private void goToMainActivity() {
+    startActivity(new Intent(this, MainActivity.class));
+    finish();
+  }
+
+  public void signIn(View view) {
     List<AuthUI.IdpConfig> providers = Arrays.asList(
       new AuthUI.IdpConfig.EmailBuilder().build(),
       new AuthUI.IdpConfig.PhoneBuilder().build(),
@@ -43,13 +53,19 @@ public class FirebaseActivity extends AppCompatActivity {
     startActivityForResult(signInIntent, RC_SIGN_IN);
   }
 
-  public void signOut() {
-    AuthUI.getInstance().
-      signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
-      @Override
-      public void onComplete(@NonNull Task<Void> task) {
-        new MainActivity();
-      }
-    });
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == RC_SIGN_IN) {
+      handleSignIn(resultCode);
+    }
+  }
+
+  private void handleSignIn(int resultCode) {
+    if (resultCode == RESULT_OK) {
+      goToMainActivity();
+    } else {
+      Toast.makeText(this, "Could not Sign in", Toast.LENGTH_SHORT).show();
+    }
   }
 }
