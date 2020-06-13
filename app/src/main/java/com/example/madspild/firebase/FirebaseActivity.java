8a1232a8216eac4c739e6f79886edf11a44ae228
armin <armin2013@live.dk>
2020-06-13 14:57:26 +0200

    Removed "Sign in" button

--- app/src/main/java/com/example/madspild/firebase/FirebaseActivity.java
+++ app/src/main/java/com/example/madspild/firebase/FirebaseActivity.java
@@ -2,7 +2,6 @@ package com.example.madspild.firebase;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.madspild.MainActivity;
 import com.example.madspild.R;
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -27,20 +25,6 @@ public class FirebaseActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     checkIfSignedIn();
     setContentView(R.layout.firebase_sign_in);
-  }
-
-  private void checkIfSignedIn() {
-    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    if (user != null)
-      goToMainActivity();
-  }
-
-  private void goToMainActivity() {
-    startActivity(new Intent(this, MainActivity.class));
-    finish();
-  }
-
-  public void signIn(View view) {
     List<AuthUI.IdpConfig> providers = Arrays.asList(
       new AuthUI.IdpConfig.EmailBuilder().build(),
       new AuthUI.IdpConfig.PhoneBuilder().build(),
@@ -52,6 +36,17 @@ public class FirebaseActivity extends AppCompatActivity {
       .build();
 
     startActivityForResult(signInIntent, RC_SIGN_IN);
+  }
+
+  private void checkIfSignedIn() {
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    if (user != null)
+      goToMainActivity();
+  }
+
+  private void goToMainActivity() {
+    startActivity(new Intent(this, MainActivity.class));
+    finish();
   }
 
   @Override
